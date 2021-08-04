e44383a07b92a596e3816eb9d41975b51e72e328
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-08-04 21:21:17 +0700

    ADD PG

--- db/db.go
+++ db/db.go
@@ -1,10 +1,13 @@
 package db
 
 import (
-	"gorm.io/driver/sqlite"
+	"fmt"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"os"
 )
 
 func GetDB() (*gorm.DB, error) {
-	return gorm.Open(sqlite.Open("todo.db"), &gorm.Config{})
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable", os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"))
+	return gorm.Open(postgres.Open(dsn), &gorm.Config{})
 }
--- go.mod
+++ go.mod
@@ -17,6 +17,7 @@ require (
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gorm.io/driver/postgres v1.1.0
 	gorm.io/driver/sqlite v1.1.4
 	gorm.io/gorm v1.21.12
 )
